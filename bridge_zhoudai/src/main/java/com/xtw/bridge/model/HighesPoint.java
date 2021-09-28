16f470042f1993f5d7faf6154bac05be74c5acb1
CT <41407275+17734812911@users.noreply.github.com>
2021-09-28 17:30:09 +0800

    Delete bridge_zhoudai directory

--- bridge_zhoudai/src/main/java/com/xtw/bridge/model/HighesPoint.java
+++
@@ -1,9 +0,0 @@
-package com.xtw.bridge.model;
-
-import lombok.Data;
-
-@Data
-public class HighesPoint {
-    private String x;
-    private String y;
-}
--- bridge_zhoudai/src/main/java/com/xtw/bridge/model/Point.java
+++
@@ -1,9 +0,0 @@
-package com.xtw.bridge.model;
-
-import lombok.Data;
-
-@Data
-public class Point {
-    private String x;
-    private String y;
-}
